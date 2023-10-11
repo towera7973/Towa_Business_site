a4b25fe611a1eed7cdbb95667e057d45c6b81f43
towera7973 <56852117+towera7973@users.noreply.github.com>
2023-10-11 12:57:46 +0000

    Added render templates and jsonfy

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+Projects = [{
+    "id": "1",
+    "title": "QuardCopter ",
+    "description": "Don't bother typing “lorem ipsum” ",
+    "image":
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fbackground&psig=AOvVaw1Z_v7q_Y--lW_8_0DqQYXW&ust=1644415676461000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjR_YS_kfQCFQAAAAAdAAAAABAD",
+    "link":
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fbackground&psig=AOvVaw1Z_v7q_Y--lW_8_0DqQYXW&ust=1644415676461000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjR_YS_kfQCFQAAAAAdAAAAABAD",
+    "tags": ["python", "flask", "html", "css"]
+}, {
+    "id": "2",
+    "title": "Biogas Stove ",
+    "description": "into Google translate. If you already tried",
+    "image":
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fbackground&psig=AOvVaw1Z_v7q_Y--lW_8_0DqQYXW&ust=1644415676461000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjR_YS_kfQCFQAAAAAdAAAAABAD",
+    "link":
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fbackground&psig=AOvVaw1Z_v7q_Y--lW_8_0DqQYXW&ust=1644415676461000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjR_YS_kfQCFQAAAAAdAAAAABAD",
+    "tags": ["python", "flask", "html", "css"]
+}, {
+    "id": "3",
+    "title": "IOT PV Voltage moniter ",
+    "description": "you may have gotten anything from ",
+    "image":
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fbackground&psig=AOvVaw1Z_v7q_Y--lW_8_0DqQYXW&ust=1644415676461000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjR_YS_kfQCFQAAAAAdAAAAABAD",
+    "link":
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fbackground&psig=AOvVaw1Z_v7q_Y--lW_8_0DqQYXW&ust=1644415676461000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjR_YS_kfQCFQAAAAAdAAAAABAD",
+    "tags": ["python", "flask", "html", "css"]
+}]
+
 
 @app.route("/")
-def hello_world():
-  return "<p>Hello, World!</p>"
+def home_Page():
+  return render_template('home.html', projects=Projects)
+
+
+@app.route("/api/projects")
+def get_projects():
+  return jsonify(Projects)
 
 
 if __name__ == "__main__":
